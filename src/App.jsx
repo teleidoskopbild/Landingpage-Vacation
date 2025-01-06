06f5a63c9d4a610b6e805c155dac12fc99caa6fd
teleidoskopbild <teleidoskopbild@gmail.com>
2025-01-06 22:17:50 +0100

    gh-pages hotfix

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="landing-page">
       <video autoPlay loop muted className="background-video">
-        <source src="public/background.mp4" type="video/mp4" />
+        <source src="background.mp4" type="video/mp4" />
       </video>
       <div className="content">
         <h1>DISCOVER</h1>
