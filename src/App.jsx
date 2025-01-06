cd016caa3193913fd8c965fc8ebc11078b047b35
teleidoskopbild <teleidoskopbild@gmail.com>
2025-01-06 20:56:05 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="landing-page">
+      <video autoPlay loop muted className="background-video">
+        <source src="/public/background.mp4" type="video/mp4" />
+      </video>
+      <div className="content">
+        <h1>DISCOVER</h1>
+        <h2>THE WORLD</h2>
+        <p>Find your dream vacation</p>
+        <button>Get Started</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
